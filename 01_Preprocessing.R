ecd82721e3eedda4e6c6b2a89a69fc41456ff3c9
LuPaschen <LuPaschen@users.noreply.github.com>
2024-06-28 17:00:36 +0200

    Add files via upload

---
+++ 01_Preprocessing.R
@@ -0,0 +1,137 @@
+# Preprocessing script for Paschen (20xx): Homophones
+
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(readr)
+library(purrr)
+
+
+
+### 1) Read CSV files from DoReCo 1.3 and gloss equivalence tables
+setwd("C:/ling/00_projects/Homophony")
+gl_csv_dir = "C:/ling/00_projects/Homophony/gl_csv"
+doreco_csv_dir = "C:/ling/00_projects/Homophony/ph_csv"
+gl_csv_files <- list.files(path = gl_csv_dir, pattern = "Gloss equivalence sets", full.names = TRUE, recursive = TRUE)
+doreco_csv_files <- list.files(path = doreco_csv_dir, pattern = "\\_ph.csv$", full.names = TRUE, recursive = TRUE)
+gl_csv_data <- map_df(gl_csv_files, readr::read_csv)
+doreco_csv_data <- map_df(doreco_csv_files, ~read_csv(.x, col_types = cols(speaker = col_character())))
+
+
+
+### 2) Add new columns and do basic cleaning
+doreco_data_preprocessed <- doreco_csv_data %>%
+  # Remove columns not needed for analysis
+  select(-ref,-tx,-ft,-core_extended,-"doreco-mb-algn","mc-zero",refind,isnref) %>%
+  # Phone duration
+  mutate(ph_duration = end - start) %>%
+  # Segmental context
+  mutate(segmental_context = ifelse(wd_ID != lead(wd_ID), "#", lead(ph))) %>%
+  # Morph duration, morph type, base morph form
+  group_by(lang, mb_ID) %>%
+  mutate(mb_duration = last(end) - first(start),
+         mb_size = n(),
+         mb_raw = str_replace_all(tolower(mb), "[\\-=~]", ""),
+         mt = case_when(
+           mb == "" ~ NA,
+           startsWith(mb, "<") ~ NA,
+           startsWith(mb, "*") ~ NA,
+           startsWith(mb, "-") & endsWith(mb, "-") ~ "infix",
+           startsWith(mb, "-") ~ "suffix",
+           endsWith(mb, "-") ~ "prefix",
+           startsWith(mb, "~") ~ "suffix",
+           endsWith(mb, "~") ~ "prefix",
+           startsWith(mb, "=") ~ "enclitic",
+           endsWith(mb, "=") ~ "proclitic",
+           is.na(mb) ~ NA,
+           TRUE ~ "root")) %>%
+  # Word duration, word size (and word-initial position - not needed)
+  group_by(lang, wd_ID) %>%
+  mutate(wd_duration = last(end) - first(start),
+         wd_size_raw = n(),
+         wd_size = wd_size_raw - mb_size) %>%
+  # IPU's, IPU-final position
+  group_by(lang, file, speaker) %>%
+  mutate(is_pause = ifelse(ph == "<p:>", TRUE, FALSE),
+         ipu_ID_perspeaker = cumsum(is_pause),
+         position_in_ipu = ifelse(lead(ph) == "<p:>", "final", "non-final")) %>%
+  ungroup() %>%
+  mutate(change = c(1, diff(ipu_ID_perspeaker) != 0),
+         ipu_ID = cumsum(change)) %>%
+  # Speech rate (not counting pauses, labels, and adjusting for auto-correlation)
+  group_by(ipu_ID) %>%
+  mutate(ipu_duration = last(end) - first(start),
+         number_pauses_and_labels = sum(grepl("<p:>|^<<", wd), na.rm = TRUE),
+         total_duration_pauses_and_labels = sum(ph_duration[grepl("<p:>|^<<", wd)], na.rm = TRUE),
+         adjusted_ipu_duration = ipu_duration - total_duration_pauses_and_labels - mb_duration,
+         speech_rate = (n()-number_pauses_and_labels-mb_size) / adjusted_ipu_duration) %>%
+  # Remove pauses and labels
+  filter(!grepl("<p:>|^<<", wd)) %>%
+  # Remove temporary columns
+  select(-mb_size,-wd_size_raw,-is_pause,-change,-ipu_ID_perspeaker,-number_pauses_and_labels,-total_duration_pauses_and_labels,-adjusted_ipu_duration) %>%
+  ungroup()
+
+
+
+### 3) Standardizing glosses based on pre-defined gloss equivalence sets
+gl_pivot <- gl_csv_data %>%
+  pivot_longer(cols = -c(gl, lang), names_to = "variable", values_to = "value")
+
+matches <- gl_pivot %>%
+  filter(value %in% doreco_data_preprocessed$gl) %>%
+  select(-variable) %>%
+  distinct(lang, value, .keep_all = TRUE)
+
+doreco_data_gloss_equivalence_applied <- doreco_data_preprocessed %>%
+  left_join(matches, by = c("gl" = "value", "lang")) %>%
+  mutate(gl_final = coalesce(gl.y, as.character(gl))) %>%
+  select(-gl,-gl.y) %>%
+  rename(gl = gl_final)
+
+
+
+### 4) Further preprocessing related to homophones
+homophone_data <- doreco_data_gloss_equivalence_applied %>%
+  # Word frequency
+  group_by(lang, wd) %>%
+  mutate(word_and_glosses = paste(wd, paste(unique(gl), collapse = " "), sep = " ")) %>%   
+  group_by(lang, word_and_glosses) %>%
+  mutate(wd_freq = n_distinct(wd_ID)) %>%
+  # Remove morphs and glosses with filler content
+  filter(mb != "****", gl != "****", gl != "NC", gl != "nc") %>%
+  # Remove rows containing NAs (from monomorphemic IPU's)
+  filter(!is.na(speech_rate)) %>%
+  # Remove morph/gloss pairs with fewer than 10 occurrences
+  group_by(lang, mb_raw, gl) %>%
+  mutate(mb_freq = n_distinct(mb_ID)) %>%
+  filter(mb_freq >= 10) %>%
+  # Exclude glosses that group together various unrelated meanings
+  filter(!(lang == "savo1255" & (gl %in% c("DET.SG.M/DET.PL", "stand/INGR")))) %>%
+  filter(!(lang == "ruul1235" & (gl %in% c("1-")))) %>%
+  filter(!(lang == "sanz1248" & (gl %in% c("-PL")))) %>%
+  filter(!(lang == "nort2641" & (gl %in% c("ADP", "POP")))) %>%
+  # Determine homogeneity in morph types
+  group_by(lang, mb_raw) %>%
+  mutate(homogeneity_mt = if_else(n_distinct(mt) == 1, "Homogeneous", "Heterogeneous")) %>%
+  # Remove morphs with only one distinct gloss
+  mutate(n_homophones = n_distinct(gl)) %>%
+  filter(n_homophones > 1) %>%
+  # Restructure dataframe to one row = one morph
+  group_by(lang, mb_ID) %>%
+  filter(ph_ID == last(ph_ID)) %>%
+  ungroup()
+
+
+
+
+### 5) Replace glottocodes with human-readable language names
+glottocodes <- c("anal1239", "apah1238", "arap1274", "bain1259", "beja1238", "bora1263", "cabe1245", "cash1254", "dolg1241", "even1259", "goem1240", "goro1270", "hoch1243", "jeha1242", "jeju1234", "kaka1265", "kama1351", "kark1256", "komn1238", "ligh1234", "lowe1385", "movi1243", "ngal1292", "nisv1234", "nngg1234", "nort2641", "nort2875", "orko1234", "pnar1238", "port1286", "resi1247", "ruul1235", "sadu1234", "sanz1248", "savo1255", "sout2856", "sout3282", "stan1290", "sumi1235", "svan1243", "taba1259", "teop1238", "texi1237", "trin1278", "tsim1256", "urum1249", "vera1241", "warl1254", "yong1270", "yuca1254", "yura1255", "toto1304", "guri1247")
+language_names <- c("Anal", "Yali", "Arapaho", "Baïnounk Gubëeher", "Beja", "Bora", "Cabécar", "Cashinahua", "Dolgan", "Evenki", "Goemai", "Gorwaa", "Hoocąk", "Jahai", "Jejuan", "Kakabe", "Kamas", "Tabaq", "Komnzo", "Light Warlpiri", "Lower Sorbian", "Movima", "Dalabon", "Nisvai", "Nǁng", "Northern Kurdish (Kurmanji)", "Northern Alta", "Fanbyak", "Pnar", "Daakie", "Resígaro", "Ruuli", "Sadu", "Sanzhi Dargwa", "Savosavo", "Nafsan (South Efate)", "English (Southern England)", "French (Swiss)", "Sümi", "Svan", "Tabasaran", "Teop", "Texistepec Popoluca", "Mojeño Trinitario", "Asimjeeg Datooga", "Urum", "Vera'a", "Warlpiri", "Yongning Na", "Yucatec Maya", "Yurakaré", "Totoli", "Gurindji")
+mapping_vector <- setNames(language_names, glottocodes)
+homophone_data$lang <- mapping_vector[homophone_data$lang]
+
+
+
+### 6) Save object
+save.image(".RData")
+### saveRDS(homophone_data, "01_Preprocessing_Output.rds")
---
+++ 02_Models_and_figures.R
@@ -0,0 +1,213 @@
+# Models and figures for Paschen (20xx): Homophones
+
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(readr)
+library(randomForest)
+library(ggplot2)
+library(lingtypology)
+
+# Loading workspace
+setwd("C:/ling/00_projects/Homophony")
+load(".RData")
+### homophone_data <- readRDS("01_Preprocessing_Output.rds")
+
+
+
+### 7) Random Forest models
+
+# Prepare empty dataframe for results
+results_all_purity <- data.frame(lang = character(), morph = character(), factor = character(), contribution = numeric(), r_sq = numeric())
+
+# List of languages
+languages <- unique(homophone_data$lang)
+
+# Factors for the Random Forest models
+factors <- c("gl", "position_in_ipu", "wd_size", "speech_rate", "speaker", "wd_freq", "segmental_context")
+predictor <- "mb_duration"
+
+# Loop to run Random Forests for each homophone set, per language
+set.seed(44)
+for(l in languages) {
+
+  morphs_raw <- unique(homophone_data$mb_raw[homophone_data$lang == l])
+  for(m in morphs_raw) {
+    print(paste(l,m)) # debugging
+    data_subset <- homophone_data %>% filter(lang == l, mb_raw == m) %>% select(c(all_of(factors)), all_of(predictor))
+    if(nrow(data_subset) > 0){
+    
+      # Run model
+      rf <- randomForest(mb_duration ~ ., ntree = 500, mtry = 3, data = data_subset, importance = TRUE)
+      
+      # Model statistics
+      r_sq = max(rf$rsq)
+      contributions_purity <- rf$importance[, "IncNodePurity"] / sum(rf$importance[, "IncNodePurity"])
+      for (f in factors) {
+        results_all_purity <- rbind(results_all_purity, data.frame(lang = l, morph = m, factor = f, contribution = contributions_purity[[f]], r_sq = r_sq))
+
+      }
+    }
+  }
+}
+
+# Rename columns for better readability
+results_all_purity <- results_all_purity %>%
+  mutate(factor = case_when(
+    factor == "gl"  ~ "morph",
+    factor == "position_in_ipu" ~ "position",
+    factor == "wd_freq" ~ "word_frequency",
+    factor == "wd_size" ~ "word_size",
+    TRUE ~ factor
+  ))
+
+# Remove models below a 25% performance threshold
+results_all_purity_unfiltered <- results_all_purity
+results_all_purity <- filter(results_all_purity_unfiltered, r_sq > 0.25)
+
+
+
+### 8) Figures
+
+# Subsets and re-organized dataframes for sub-studies
+results_morph_purity <- results_all_purity %>%
+  filter(factor == "morph")
+s_data <- results_all_purity %>%
+  mutate(s = ifelse(str_detect(morph, "s|z"), "with_alveolar_sibilant", "no_alveolar_sibilant"),
+         source = "Alveolar sibilant")  %>%
+  filter(factor == "morph") 
+remember_homogeneity <- homophone_data %>% select(lang, mb_raw, homogeneity_mt) %>% distinct() %>% rename(morph = mb_raw)
+mt_data <- left_join(results_all_purity, remember_homogeneity, by = c("lang", "morph")) %>% 
+  filter(factor == "morph") %>% 
+  mutate(source = "Homogeneity")
+proportion_homo <- mean(mt_data$homogeneity_mt == "Homogeneous")
+proportion_s <- mean(s_data$s == "with_alveolar_sibilant")
+
+# Figure: Factor x Contribution (over all languages)
+contr_factor <- ggplot(results_all_purity, aes(x = reorder(factor, contribution, FUN = mean), y = contribution)) +
+  stat_summary(fun = mean, geom = "point", shape = 18, color = "purple", size = 9) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.25, color = "purple") +
+  #  geom_hline(yintercept = 0.142, linetype = "dashed", color = "black") +
+  coord_flip() +
+  theme_minimal() +
+  labs(x = "Factor", y = "Importance") +
+  theme(text = element_text(size = 32))
+ggsave(filename = "contr_factor_37_44.png", plot = contr_factor, width = 10, height = 8, units = "in", dpi = 300)
+
+# Figure: Language x Contribution of morph
+contrmorph_language <- ggplot(results_morph_purity, aes(x = reorder(lang, contribution, FUN = mean), y = contribution)) +
+  stat_summary(fun = mean, geom = "point", shape = 18, color = "purple", size = 9) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.25, color = "purple") +
+  coord_flip() +
+  theme_minimal() +
+  labs(x = "Language", y = "Importance of morph") +
+  theme(text = element_text(size = 32))
+ggsave(filename = "contrmorph_language_37_44.png", plot = contrmorph_language, width = 10, height = 12, units = "in", dpi = 300)
+
+# Figure: Crowdedness
+homophone_count <- homophone_data %>% select(lang, mb_raw, n_homophones) %>% distinct() %>% rename(morph = mb_raw)
+homophone_data_with_counts <- merge(results_morph_purity, homophone_count, by = c("lang", "morph"))
+crowdedness_contrmorph <- ggplot(data= homophone_data_with_counts, aes(x = n_homophones, y = contribution)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "purple", linewidth = 3) +
+  labs(x = "Crowdedness", y = "Importance of morph") +
+  xlim(2,6) +
+  ylim(0,0.175)+
+  theme(text = element_text(size = 32))
+ggsave(filename = "crowdedness_contrmorph_37_44.png", plot = crowdedness_contrmorph, width = 10, height = 10, units = "in", dpi = 300)
+r_crowdedness <- cor(homophone_data_with_counts$contribution, homophone_data_with_counts$n_h)
+model <- lm(contribution ~ n_homophones, data = homophone_data_with_counts)
+p_crowdedness <- summary(model)$coefficients[2, 4]
+
+# Figure: Morphs containing "s" or "z"
+contr_fact_s <- ggplot(s_data, aes(x = reorder(factor, contribution, FUN = mean), y = contribution, color = s)) +
+  stat_summary(fun = mean, geom = "point", shape = 18, size = 9) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.25) +
+  scale_color_manual(values = c("purple", "orange"))  +
+  coord_flip() +
+  theme_minimal() +
+  labs(x = "Factor", y = "Importance") +
+  theme(text = element_text(size = 32))
+ggsave(filename = "contr_factor_s_37_44.png", plot = contr_fact_s, width = 12, height = 4, units = "in", dpi = 300)
+
+# Figure: Homogeneity
+contr_fact_mt <- ggplot(mt_data, aes(x = reorder(factor, contribution, FUN = mean), y = contribution, color = homogeneity_mt)) +
+  stat_summary(fun = mean, geom = "point", shape = 18, size = 9) +
+  stat_summary(fun.data = mean_se, geom = "errorbar", width = 0.25) +
+  scale_color_manual(values = c("purple", "orange"))  +
+  coord_flip() +
+  theme_minimal() +
+  labs(x = "Factor", y = "Importance") +
+  theme(text = element_text(size = 32))
+ggsave(filename = "contr_factor_mt_37_44.png", plot = contr_fact_mt, width = 12, height = 4, units = "in", dpi = 300)
+
+
+
+### 9) Descriptive statistics
+
+# Identify the top 20 homophone sets in terms of morph variable importance
+top_20 <- results_morph_purity %>%
+  arrange(desc(contribution)) %>%
+  slice_head(n = 20)
+
+# Table 1: Number of words, homophone sets etc.
+n_words_per_lang <- doreco_data_preprocessed %>% group_by(lang) %>% summarize(word_tokens = n_distinct(wd_ID))
+n_homophonesets_per_lang <- results_all_purity %>% group_by(lang) %>% summarize(homophone_sets = n_distinct(morph))
+n_homophonesets_total <- sum(n_homophonesets_per_lang$homophone_sets)
+n_words_total <- sum(n_words_per_lang$word_tokens)
+
+# Crowdedness: Number of homophone sets with n members
+crowdedness_table <- table(homophone_data_with_counts$n_homophones)
+
+# For the top 20 homophone sets, calculate mean morph durations
+unique_combinations <- top_20 %>%
+  select(lang, morph) %>%
+  distinct()
+mean_durations <- homophone_data %>%
+  group_by(lang, mb_raw, gl) %>%
+  summarize(
+    mean_duration = 1000*mean(mb_duration, na.rm = TRUE),
+    mb = first(mb),
+    .groups = 'drop') %>%
+  rename(morph = mb_raw)
+top_20_with_means <- unique_combinations %>%
+  left_join(mean_durations, by = c("lang", "morph"))
+
+# Identify smallest/ largest homophone sets
+hom_set_counts <- homophone_data %>%
+  rename(morph = mb_raw) %>%
+  semi_join(results_all_purity, by = c("lang", "morph")) %>%
+  group_by(lang, morph) %>%
+  reframe(count = n(), n_homophones = n_homophones) %>%
+  arrange(count) %>%
+  distinct()
+
+smallest_hom_sets <- hom_set_counts %>%
+  filter(count == min(count))
+
+largest_hom_sets <- hom_set_counts %>%
+  filter(count == max(count))
+
+
+### 10) Map
+
+languages_map <- data.frame(lang = c("Arapaho", "Bainounk-Gujaher", "Beja", "Bora", "Cabécar", "Cashinahua", "Daakie", "Ngalkbun", "Dolgan", "Evenki", "Orkon-Fanbak", "Goemai", "Gorowa", "Ho-Chunk", "Jehai", "Jejueo", "Kakabe", "Kamas-Koibal", "Trinitario-Javeriano-Loretano", "Movima", "Nafsan", "Nisvai", "N||ng", "Northern Alta", "Northern Kurdish", "Pnar", "Ruuli", "Sanzhi-Icari", "Savosavo", "Sumi Naga", "Tabasaran", "Teop", "Texistepec Popoluca", "Totoli", "Kipchak Urum", "Vera'a", "Angguruk Yali"),
+                            macro_area = c("NAm", "Afr", "Afr", "SAm", "NAm", "SAm", "Pap", "Aus", "Eur", "Eur", "Pap", "Afr", "Afr", "NAm", "Eur", "Eur", "Afr", "Eur", "SAm", "SAm", "Pap", "Pap", "Afr", "Pap", "Eur", "Eur", "Afr", "Eur", "Pap", "Eur", "Eur", "Pap", "NAm", "Pap", "Eur", "Pap", "Pap"))
+
+map.feature(languages = languages_map$lang,
+            features= languages_map$macro_area,
+            tile = "Esri.WorldGrayCanvas",
+            color = c("purple", "blue", "red", "black", "green", "orange"),
+            legend = FALSE)
+
+
+
+### 11) Housekeeping
+
+# Save workspace
+save.image(".RData")
+
+# Generate citations
+citation()
+packages <- c("dplyr", "tidyverse", "stringr", "readr", "randomForest", "ggplot2", "lingtypology", "purr")
+lapply(packages, citation)
